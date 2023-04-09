9e6655dec6d615306122253473525c72d3592a21
Luiz Fernando <105685208+dffariac@users.noreply.github.com>
2023-04-09 09:51:32 -0300

    Delete ILutador.java

--- ILutador.java
+++
@@ -1,8 +0,0 @@
-public interface ILutador {
-
-public abstract  void Apresentar ();
-public abstract void Status ();
-public abstract void GanharLuta ();
-public abstract void EmpatarLuta();
-public abstract void PerderLuta ();
-}
